b434346d84aa88701988704b565452369a8ab965
xiedong <xiedong11@aliyun.com>
2019-01-18 18:24:35 +0800

    添加个人管理路由路由

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router,Route} from 'react-router-dom';
+import { HashRouter as Router,Route} from 'react-router-dom';
 import Home from './pages/home.jsx'
 import PersonManage from './pages/personManage.jsx'
 
@@ -9,6 +9,7 @@ class App extends Component {
             <Router>
                 <div>
                     <Route exact path="/" component={Home}/>
+                    <Route  path="/personManage" component={PersonManage}/>
 
                 </div>
             </Router>
