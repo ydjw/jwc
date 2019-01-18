d17d0d1e4ebcea9d2a3851e31ecca7c4cd2d1968
xiedong <xiedong11@aliyun.com>
2019-01-18 16:08:11 +0800

    暂存代码

--- src/App.js
+++ src/App.js
@@ -1,39 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import HomeMenu from './components/HomeMenu.jsx'
-import personManageImg from "./img/ic_people_black_48dp.png"
-import xuankeImg from "./img/social.png"
-import jiaoxueImg from "./img/ic_domain_black_48dp.png"
-import kaowuImg from "./img/ic_event_note_black_48dp.png"
-import zongheImg from "./img/ic_quick_contacts_dialer_black_48dp.png"
-import jiaoxuepingguImg from "./img/ic_local_library_black_48dp.png"
-import { Toast } from 'antd-mobile';
+import { BrowserRouter as Router,Route} from 'react-router-dom';
+import Home from './pages/home.jsx'
+import Page1 from './pages/Page1.jsx'
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <div id="header-container"></div>
-                <div id="menu-item">
-                    <HomeMenu menuTitle="个人管理"
-                              menuImg={personManageImg}></HomeMenu>
-                    <HomeMenu menuTitle="选课管理"
-                              menuImg={xuankeImg}></HomeMenu>
+            <Router>
+                <div>
+                    <Route path="'/" component={Home}/>
+                    <Route path="'/Page1" component={Page1}/>
                 </div>
-                <div id="menu-item">
-                    <HomeMenu menuTitle="教学资源"
-                              menuImg={jiaoxueImg}></HomeMenu>
-                    <HomeMenu menuTitle="考务管理"
-                              menuImg={kaowuImg}></HomeMenu>
-                </div>
-                <div id="menu-item">
-                    <HomeMenu menuTitle="综合查询"
-                              menuImg={zongheImg} ></HomeMenu>
-                    <HomeMenu menuTitle="教学评价"
-                              menuImg={jiaoxuepingguImg}></HomeMenu>
-                </div>
-            </div>
+            </Router>
         );
     }
 }
---
+++ src/pages/home.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './css/home.css';
+import HomeMenu from '../components/HomeMenu.jsx'
+import personManageImg from "../img/ic_people_black_48dp.png"
+import xuankeImg from "../img/social.png"
+import jiaoxueImg from "../img/ic_domain_black_48dp.png"
+import kaowuImg from "../img/ic_event_note_black_48dp.png"
+import zongheImg from "../img/ic_quick_contacts_dialer_black_48dp.png"
+import jiaoxuepingguImg from "../img/ic_local_library_black_48dp.png"
+import { Toast } from 'antd-mobile';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="App">
+                <div id="header-container"></div>
+                <div id="menu-item">
+                    <HomeMenu menuTitle="个人管理"
+                              menuImg={personManageImg}></HomeMenu>
+                    <HomeMenu menuTitle="选课管理"
+                              menuImg={xuankeImg}></HomeMenu>
+                </div>
+                <div id="menu-item">
+                    <HomeMenu menuTitle="教学资源"
+                              menuImg={jiaoxueImg}></HomeMenu>
+                    <HomeMenu menuTitle="考务管理"
+                              menuImg={kaowuImg}></HomeMenu>
+                </div>
+                <div id="menu-item">
+                    <HomeMenu menuTitle="综合查询"
+                              menuImg={zongheImg} ></HomeMenu>
+                    <HomeMenu menuTitle="教学评价"
+                              menuImg={jiaoxuepingguImg}></HomeMenu>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
