d41c48e93500657690ecf3ab3a7f29e3b34922e1
xiedong <xiedong11@aliyun.com>
2019-01-22 18:08:31 +0800

    移植bmob

--- src/pages/personManage.jsx
+++ src/pages/personManage.jsx
@@ -1,10 +1,34 @@
-import React, { Component } from 'react';
-
+import React, {Component} from 'react';
+import bmob from "hydrogen-js-sdk";
+import util from '../util/util'
 class PersonManage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            result: {}
+        }
+        bmob.initialize('67be1cdcf5559af1366b88f688f56473', 'd522e6953340491e6cec8e840543183b')
+    }
+
+    componentDidMount() {
+        let aa= util.getSearchByName("id")
+        if (aa!=null){
+            bmob.User.login(aa, aa).then(res => {
+                this.setState({
+                    result: res
+                })
+            }).catch(err => {
+            })
+        }
+
+    }
+
     render() {
+        let {result} = this.state
         return (
             <div>
-                fsdsfa
+                <div>{result.username}</div>
             </div>
         )
     }
