55628f95e79926f120b9592af9007a26a9a3fcb9
xiedong <xiedong11@aliyun.com>
2019-06-03 16:24:33 +0800

    完成登陆逻辑

--- src/pages/education/login/index.jsx
+++ src/pages/education/login/index.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import './style.less'
 import {Toast} from 'antd-mobile';
+import EducationApi from '../../../api/educations'
 
 
 export default class LoginPage extends Component {
@@ -17,14 +18,19 @@ export default class LoginPage extends Component {
 
     }
 
-    doLogin() {
+    async doLogin() {
         var username = document.getElementById('input-username').value;
         var password = document.getElementById('input-password').value;
         if (password.length > 6 && username.length > 8) {
-            Toast.loading('Loading...', 3, () => {
-                this.props.history.push('/home');
-                Toast.info('登陆成功...', 1);
-            });
+            Toast.loading('登陆中...', 0);
+           let res=await EducationApi.login(`/app.do?method=authUser&xh=${username}&pwd=${password}`)
+           if (res.data.success){
+               Toast.hide()
+               Toast.info("欢迎你，"+res.data.user.username,3)
+               this.props.history.push('/home');
+           }else {
+               Toast.info("登陆信息有误...",1)
+           }
         } else {
             Toast.info('请确保账号密码正确...', 2);
         }
@@ -41,7 +47,7 @@ export default class LoginPage extends Component {
                 <div id='bottom-view'>
 
                     <input id='input-username' placeholder='信息门户账号'/>
-                    <input id='input-password' placeholder='信息门户密码'/>
+                    <input id='input-password' placeholder='信息门户密码' type='password'/>
                     <div id='login-container'>
                         <span id='btn-login' onClick={this.doLogin}>戳我登陆</span>
                         <span id='forget-password'>忘记密码？</span>
